b51964636dba4971cca8a433141fad1514e28a54
Angelina335 <69483414+Angelina335@users.noreply.github.com>
2020-08-12 10:22:02 -0300

    Add files via upload

---
+++ 2b.py
@@ -0,0 +1,8 @@
+a = int(input("Digite o primeiro número: "))
+b = int(input("Digite o segundo número: "))
+c = int(input("Digite o terceiro número: "))
+
+media = (a + b + c)/3
+
+print(f'A média dos números é {media}')
+[10:01, 12/08/2020] Rikerdaves: 2 
