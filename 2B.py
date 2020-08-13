ca3ab64017c652362280bdd04bb2b5dc68234b07
Angelina335 <69483414+Angelina335@users.noreply.github.com>
2020-08-13 12:34:39 -0300

    Delete 2B.py

--- 2B.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Digite o primeiro número: "))
-b = int(input("Digite o segundo número: "))
-c = int(input("Digite o terceiro número: "))
-
-media = (a + b + c)/3
-
-print(f'A média dos números é {media}')
